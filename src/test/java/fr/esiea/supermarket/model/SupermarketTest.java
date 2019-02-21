c2ddd2314843507b3b5b924dd99b5ab00ddeb2fa
CyrilMlb <malherbe@et.esiea.fr>
2019-02-21 19:11:53 +0100

    Adding tests SupermarketTest for ShoppingCart (2)

--- src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
+++ src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
@@ -10,22 +10,36 @@ public class SupermarketTest {
         SupermarketCatalog catalog = new FakeCatalog();
         Product toothbrush = new Product("toothbrush", ProductUnit.Each);
         catalog.addProduct(toothbrush, 0.99);
+        Product hairbrush = new Product("hairbrush", ProductUnit.Each);
+        catalog.addProduct(hairbrush, 3.5);
+        Product soap = new Product("soap", ProductUnit.Each);
+        catalog.addProduct(soap, 2.5);
         Product apples = new Product("apples", ProductUnit.Kilo);
         catalog.addProduct(apples, 1.99);
+        Product bananas = new Product("bananas", ProductUnit.Kilo);
+        catalog.addProduct(bananas, 0.5);
+        Product cherries = new Product("cherries", ProductUnit.Kilo);
+        catalog.addProduct(cherries, 3.9);
+
 
         ShoppingCart cart = new ShoppingCart();
         cart.addItemQuantity(apples, 2.5);
-
         Teller teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
         Receipt receipt = teller.checksOutArticlesFrom(cart);
-
         Assertions.assertThat(receipt.getTotalPrice()).as("Price for 2.5kg of apples.").isEqualTo(2.5*1.99);
 
-        cart.addItemQuantity(apples, 1.5);
-        cart.addItem(toothbrush);
+        cart.addItemQuantity(toothbrush, 1.0);
+        cart.addItemQuantity(hairbrush, 2.0);
+        cart.addItem(soap);
+        cart.addItemQuantity(bananas, 10.0);
+        cart.addItemQuantity(cherries, 5.0);
 
-        receipt = teller.checksOutArticlesFrom(cart);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 3.0);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, cherries, 10.0);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, bananas, 2.0);
+        teller.addSpecialOffer(SpecialOfferType.FiveForAmount, soap, 5.0);
+        teller.addSpecialOffer(null, hairbrush, 0.0);
+
+		receipt = teller.checksOutArticlesFrom(cart);
     }
 }
