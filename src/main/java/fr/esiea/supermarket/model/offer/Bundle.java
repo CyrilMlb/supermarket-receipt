e2e47f5ea2b19c9eab980eaeb298a8bab1fea9b2
4kaguru <38265864+4kaguru@users.noreply.github.com>
2019-02-27 23:38:31 +0100

    Final change on Bundle.java

--- src/main/java/fr/esiea/supermarket/model/offer/Bundle.java
+++ src/main/java/fr/esiea/supermarket/model/offer/Bundle.java
@@ -6,15 +6,16 @@ import java.util.*;
 public class Bundle{
 	private HashMap<Product, Double> hmProducts;
 	
-	public Bundle(HashMap<Product, Double> hmProducts){
+	public Bundle(){
 		this.hmProducts = new HashMap<Product, Double>();
-		if(hmProducts != null){
-			this.hmProducts = new HashMap<Product, Double>(hmProducts);
-		}
 	}
 	
 	public HashMap<Product, Double> getProducts(){
 		return this.hmProducts;
+	}
+
+	public void addProduct(Product product, double quantity){
+		this.hmProducts.put(product, new Double(quantity) );
 	}
 
 	public boolean isEmpty(){
